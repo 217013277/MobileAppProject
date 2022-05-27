b038ddef0c54e0038ce30d6687c0de12379e5571
hongyiuyu <217013277@stu.vtc.edu.hk>
2022-05-27 16:56:51 +0800

    add account preference and edit LoginActivity.kt and RegisterActivity.kt

--- app/src/main/java/com/example/mobileappproject/LoginActivity.kt
+++ app/src/main/java/com/example/mobileappproject/LoginActivity.kt
@@ -3,42 +3,135 @@ package com.example.mobileappproject
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.View
+import android.widget.Button
 import android.widget.EditText
+import android.widget.TextView
 import android.widget.Toast
-import com.google.android.gms.auth.api.identity.BeginSignInRequest
-import com.google.android.gms.auth.api.identity.Identity
-import com.google.android.gms.auth.api.identity.SignInClient
+import androidx.cardview.widget.CardView
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.gms.common.SignInButton
+import com.google.android.gms.common.api.ApiException
+import com.google.android.gms.tasks.Task
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.GoogleAuthProvider
 
 class LoginActivity : AppCompatActivity() {
-    private lateinit var auth: FirebaseAuth
+    private lateinit var mGoogleSignInClient: GoogleSignInClient
+    private val Req_Code:Int=123
+    private lateinit var firebaseAuth: FirebaseAuth
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
 
-        auth = FirebaseAuth.getInstance()
+        val loginBtn = findViewById<Button>(R.id.loginBtn)
+        loginBtn.setOnClickListener{
+            login()
+        }
+
+//        findViewById<Button>(R.id.loginBtn).setOnClickListener{ login() }
+
+        val goToRegisterBtn = findViewById<TextView>(R.id.textViewToRegister)
+        goToRegisterBtn.setOnClickListener{
+            Toast.makeText(this,"go to Register Page",Toast.LENGTH_SHORT).show()
+            goToRegister()
+        }
+
+        // Configure Google Sign In inside onCreate mentod
+        val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestIdToken(getString(R.string.app_web_client_id))
+            .requestEmail()
+            .build()
+        // getting the value of gso inside the GoogleSigninClient
+        mGoogleSignInClient= GoogleSignIn.getClient(this,gso)
+        firebaseAuth = FirebaseAuth.getInstance()
+
+        val googleSignIn = findViewById<SignInButton>(R.id.googleSignInBtn)
+        googleSignIn.setOnClickListener{
+            Toast.makeText(this,"Logging In",Toast.LENGTH_SHORT).show()
+            signInGoogle()
+        }
     }
 
-    fun login(view: View){
+    // Email and password login
+    fun login() {
         val editTextEmailAddress = findViewById<EditText>(R.id.editTextEmailAddress)
         val editTextPassword = findViewById<EditText>(R.id.editTextPassword)
         val email = editTextEmailAddress.text.toString()
         val password = editTextPassword.text.toString()
-        auth.signInWithEmailAndPassword(email,password).addOnCompleteListener { task ->
+        firebaseAuth.signInWithEmailAndPassword(email,password).addOnCompleteListener { task ->
             if(task.isSuccessful){
-                val intent= Intent(this,MainActivity::class.java)
-                startActivity(intent)
+                AccountPreference.setEmail(this, firebaseAuth.currentUser?.email.toString())
+                AccountPreference.setUsername(this,firebaseAuth.currentUser?.displayName.toString())
+                goToMain()
                 finish()
             }
         }.addOnFailureListener { exception ->
             Toast.makeText(applicationContext,exception.localizedMessage, Toast.LENGTH_LONG).show()
         }
     }
-    fun goToRegister(view: View){
+
+    // Google login
+    private fun signInGoogle(){
+
+        val signInIntent: Intent =mGoogleSignInClient.signInIntent
+        startActivityForResult(signInIntent,Req_Code)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if(requestCode==Req_Code){
+            val task: Task<GoogleSignInAccount> = GoogleSignIn.getSignedInAccountFromIntent(data)
+            handleResult(task)
+//            firebaseAuthWithGoogle(account!!)
+        }
+    }
+
+    private fun handleResult(completedTask: Task<GoogleSignInAccount>){
+        try {
+            val account: GoogleSignInAccount? = completedTask.getResult(ApiException::class.java)
+            if (account != null) {
+                updateUI(account)
+            }
+        } catch (e: ApiException){
+            Toast.makeText(this,e.toString(), Toast.LENGTH_SHORT).show()
+            Log.d("ApiException",e.toString())
+        }
+    }
+
+    private fun updateUI(account: GoogleSignInAccount){
+        val credential = GoogleAuthProvider.getCredential(account.idToken,null)
+        firebaseAuth.signInWithCredential(credential).addOnCompleteListener {task->
+            if(task.isSuccessful) {
+                AccountPreference.setEmail(this,account.email.toString())
+                AccountPreference.setUsername(this,account.displayName.toString())
+                finish()
+                goToMain()
+            }
+        }
+    }
+
+    private fun goToMain(){
+        val intent= Intent(this,MainActivity::class.java)
+        startActivity(intent)
+    }
+
+    private fun goToRegister(){
         val intent= Intent(this,RegisterActivity::class.java)
         startActivity(intent)
     }
 
+    // if you do not add this check, then you would have to login everytime you start your application on your phone.
+//    override fun onStart() {
+//        super.onStart()
+//        if(GoogleSignIn.getLastSignedInAccount(this)!=null){
+//            goToMain()
+//        }
+//    }
 }
+
--- app/src/main/java/com/example/mobileappproject/RegisterActivity.kt
+++ app/src/main/java/com/example/mobileappproject/RegisterActivity.kt
@@ -5,33 +5,46 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.view.View
+import android.widget.Button
 import android.widget.EditText
+import android.widget.TextView
 import android.widget.Toast
+import com.google.android.gms.auth.api.signin.GoogleSignIn
 import com.google.firebase.auth.FirebaseAuth
 
 class RegisterActivity : AppCompatActivity() {
 
-    private lateinit var  auth: FirebaseAuth
+    private lateinit var  firebaseAuth: FirebaseAuth
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_register)
 
-        auth= FirebaseAuth.getInstance()
+        val registerBtn = findViewById<Button>(R.id.registerBtn)
+        registerBtn.setOnClickListener{
+            register()
+        }
+
+        val goToLoginBtn = findViewById<TextView>(R.id.textViewToLogin)
+        goToLoginBtn.setOnClickListener{
+            Toast.makeText(this,"go to Register Page",Toast.LENGTH_SHORT).show()
+            goToLogin()
+        }
+
+        firebaseAuth = FirebaseAuth.getInstance()
     }
 
-    fun register(view: View){
+    private fun register(){
         val editTextEmailAddress = findViewById<EditText>(R.id.editTextEmailAddress)
         val editTextPassword = findViewById<EditText>(R.id.editTextPassword)
         val email=editTextEmailAddress.text.toString()
         val password=editTextPassword.text.toString()
 
-        auth.createUserWithEmailAndPassword(email,password).addOnCompleteListener { task ->
+        firebaseAuth.createUserWithEmailAndPassword(email,password).addOnCompleteListener { task ->
             if(task.isSuccessful){
-                val intent= Intent(this,MainActivity::class.java)
-                startActivity(intent)
-                auth.currentUser
+//                firebaseAuth.currentUser
                 finish()
+                goToMain()
             }
         }.addOnFailureListener { exception ->
             Toast.makeText(applicationContext,exception.localizedMessage,Toast.LENGTH_LONG).show()
@@ -39,10 +52,23 @@ class RegisterActivity : AppCompatActivity() {
         }
     }
 
-    fun goToLogin(view: View){
+    private fun goToMain(){
+        val intent= Intent(this,MainActivity::class.java)
+        startActivity(intent)
+    }
+
+    private fun goToLogin(){
         val intent= Intent(this,LoginActivity::class.java)
         startActivity(intent)
     }
+
+    // if you do not add this check, then you would have to login everytime you start your application on your phone.
+//    override fun onStart() {
+//        super.onStart()
+//        if(GoogleSignIn.getLastSignedInAccount(this)!=null){
+//            goToLogin()
+//        }
+//    }
 
 }
 
