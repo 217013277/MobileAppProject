aad8c5f67622b5dc69c252c13c2e2dee29d8a94d
hongyiuyu <217013277@stu.vtc.edu.hk>
2022-05-31 14:27:03 +0800

    implement firebase realtime database and show to listview successfully

--- app/src/main/java/com/example/mobileappproject/MainActivity.kt
+++ app/src/main/java/com/example/mobileappproject/MainActivity.kt
@@ -2,17 +2,34 @@ package com.example.mobileappproject
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Button
-import android.widget.TextView
+import android.util.Log
+import android.view.View
+
+
+import android.widget.*
+
 import com.example.mobileappproject.extensions.goToLoginActivity
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+
 import com.google.firebase.auth.ktx.auth
+import com.google.firebase.database.*
 import com.google.firebase.ktx.Firebase
 
-class MainActivity : AppCompatActivity() {
+
+class MainActivity : AppCompatActivity(), TaskRowListener {
+
+    lateinit var _db: DatabaseReference
+    var _taskList: MutableList<Task>? = null
+    lateinit var _adapter: TaskAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        _db = FirebaseDatabase.getInstance("https://vtclab-da73a-default-rtdb.asia-southeast1.firebasedatabase.app/").reference
+        _taskList = mutableListOf()
+        _adapter = TaskAdapter(this, _taskList!!)
+        findViewById<ListView>(R.id.listviewTask)!!.setAdapter(_adapter)
 
         val email = findViewById<TextView>(R.id.email)
         val logoutBtn = findViewById<Button>(R.id.logoutBtn)
@@ -24,24 +41,79 @@ class MainActivity : AppCompatActivity() {
             email.text = getString(R.string.no_user_found)
         }
 
+        val _taskListener: ValueEventListener = object : ValueEventListener {
+            override fun onDataChange(dataSnapshot: DataSnapshot) {
+                loadTaskList(dataSnapshot)
+            }
+            override fun onCancelled(databaseError: DatabaseError) {
+                // Getting Item failed, log a message
+                Log.w("MainActivity", "loadItem:onCancelled", databaseError.toException())
+            }
+        }
+
+        _db.orderByKey().addValueEventListener(_taskListener)
+
         logoutBtn.setOnClickListener{
             Firebase.auth.signOut()
             goToLogin()
         }
     }
 
-//    private fun logout() {
-//        gsc.signOut().addOnCompleteListener(this) { task ->
-//            if(task.isSuccessful){
-//                goToLogin()
-//                finish()
-//            }
-//        }.addOnFailureListener { exception ->
-//            Toast.makeText(applicationContext,exception.localizedMessage, Toast.LENGTH_LONG).show()
-//        }
-//    }
-
     private fun goToLogin(){
         goToLoginActivity(this)
+    }
+
+    private fun loadTaskList(dataSnapshot: DataSnapshot) {
+        Log.d("MainActivity", "loadTaskList")
+        val tasks = dataSnapshot.children.iterator()
+        //Check if current database contains any collection
+        if (tasks.hasNext()) {
+            _taskList!!.clear()
+            val listIndex = tasks.next()
+            val itemsIterator = listIndex.children.iterator()
+            //check if the collection has any task or not
+            while (itemsIterator.hasNext()) {
+                //get current task
+                val currentItem = itemsIterator.next()
+                val task = Task.create()
+                //get current data in a map
+                val map = currentItem.value as HashMap<String, Any>
+                //key will return the Firebase ID
+                task.objectId = currentItem.key
+                task.done = map.get("done") as Boolean?
+                task.taskDesc = map.get("taskDesc") as String?
+                _taskList!!.add(task)
+            }
+        }
+        //alert adapter that has changed
+        _adapter.notifyDataSetChanged()
+    }
+
+//    fun addTask(){
+//        //Declare and Initialise the Task
+//        val task = Task.create()
+//        //Set Task Description and isDone Status
+//        task.taskDesc = findViewById<EditText>(com.google.firebase.database.R.id.txtNewTaskDesc).text.toString()
+//        task.done = false
+//        //Get the object id for the new task from the Firebase Database
+//        val newTask = _db.child(Statics.FIREBASE_TASK).push()
+//        task.objectId = newTask.key
+//        //Set the values for new task in the firebase using the footer form
+//        newTask.setValue(task)
+//        //Hide the footer and show the floating button
+//        findViewById<RelativeLayout>(com.google.firebase.database.R.id.footer).visibility = View.GONE
+//        findViewById<FloatingActionButton>(com.google.firebase.database.R.id.fab).visibility = View.VISIBLE
+//        //Reset the new task description field for reuse.
+//        findViewById<EditText>(com.google.firebase.database.R.id.txtNewTaskDesc).setText("")
+//        Toast.makeText(this, "Task added to the list successfully" + task.objectId, Toast.LENGTH_SHORT).show()
+//    }
+
+    override fun onTaskChange(objectId: String, isDone: Boolean) {
+        val task = _db.child(Statics.FIREBASE_TASK).child(objectId)
+    }
+
+    override fun onTaskDelete(objectId: String) {
+        val task = _db.child(Statics.FIREBASE_TASK).child(objectId)
+        task.removeValue()
     }
 }
