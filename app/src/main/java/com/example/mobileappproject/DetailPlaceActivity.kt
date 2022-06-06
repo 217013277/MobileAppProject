6d6bb8dd70a6076dec226434190f81b7ddf5d821
yiuyu <217013277@stu.vtc.edu.hk>
2022-06-07 01:08:09 +0800

    show detail page information successfully

--- app/src/main/java/com/example/mobileappproject/DetailPlaceActivity.kt
+++ app/src/main/java/com/example/mobileappproject/DetailPlaceActivity.kt
@@ -1,8 +1,12 @@
 package com.example.mobileappproject
 
 import android.os.Bundle
+import android.text.SpannableString
+import android.text.style.UnderlineSpan
 import android.util.Log
 import android.widget.Button
+import android.widget.ImageView
+import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.example.mobileappproject.extensions.goToMainActivity
@@ -10,15 +14,25 @@ import com.example.mobileappproject.lists.PlaceStatics
 import com.example.mobileappproject.lists.Task
 import com.google.firebase.database.DatabaseReference
 import com.google.firebase.database.FirebaseDatabase
+import com.squareup.picasso.Picasso
 import kotlin.reflect.typeOf
 
 class DetailPlaceActivity : AppCompatActivity() {
 
-    lateinit var _db: DatabaseReference
+    private lateinit var detailLat: String
+    private lateinit var detailLon: String
+    private lateinit var imageUrl: String
+    private lateinit var _db: DatabaseReference
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_detail_place)
+
+        val detailMainImage = findViewById<ImageView>(R.id.ivMainImage)
+        val detailName = findViewById<TextView>(R.id.tvDetailName)
+        val detailDesc = findViewById<TextView>(R.id.tvDetailDesc)
+        val detailAddress = findViewById<TextView>(R.id.tvDetailAddress)
+        val detailWeather = findViewById<TextView>(R.id.tvDetailWeather)
 
         _db = FirebaseDatabase.getInstance(getString(R.string.firebase_realtime_database_url)).reference
         // Get parameter from last Activity
@@ -27,6 +41,24 @@ class DetailPlaceActivity : AppCompatActivity() {
             if (id != null) {
                 Toast.makeText(this, id, Toast.LENGTH_SHORT).show()
                 _db.child(PlaceStatics.FIREBASE_PLACE).child(id).get().addOnSuccessListener {
+                    detailName.text = it.child("placeName").value.toString()
+                    detailDesc.text = it.child("placeDesc").value.toString()
+//                    detailAddress.text = it.child("placeAddress").value.toString()
+                    val address = it.child("placeAddress").value.toString()
+                    // Creating a Spannable String
+                    // from the above string
+                    val addressUnderline = SpannableString(address)
+                    // Setting underline style from
+                    // position 0 till length of
+                    // the spannable string
+                    addressUnderline.setSpan(UnderlineSpan(), 0, addressUnderline.length, 0)
+                    detailAddress.text = addressUnderline
+                    detailWeather.text = it.child("placeWeather").value.toString()
+                    detailLat = it.child("placeLatitude").value.toString()
+                    detailLat = it.child("placeLongitude").value.toString()
+                    imageUrl = it.child("imageUrl").value.toString()
+                    Picasso.get().load(imageUrl).into(detailMainImage)
+
                     Log.i("firebase", "Got value ${it.value}")
                 }
             } else {
