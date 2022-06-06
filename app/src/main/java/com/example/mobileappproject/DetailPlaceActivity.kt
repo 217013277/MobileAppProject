116672aaf824d45cdd68759e89f4e679ce8d7fe0
hongyiuyu <217013277@stu.vtc.edu.hk>
2022-06-06 17:05:36 +0800

    create DetailPlaceActivity.kt

--- app/src/main/java/com/example/mobileappproject/DetailPlaceActivity.kt
+++ app/src/main/java/com/example/mobileappproject/DetailPlaceActivity.kt
@@ -3,8 +3,13 @@ package com.example.mobileappproject
 import android.os.Bundle
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
 
 class DetailPlaceActivity : AppCompatActivity() {
+
+    lateinit var _db: DatabaseReference
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_detail_place)
@@ -16,5 +21,7 @@ class DetailPlaceActivity : AppCompatActivity() {
                 Toast.makeText(this, "Cannot not find the object id", Toast.LENGTH_SHORT).show()
             }
         }
+
+        _db = FirebaseDatabase.getInstance(getString(R.string.firebase_realtime_database_url)).reference
     }
 }
