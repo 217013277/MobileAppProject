7eb2ae525a286e49b84681b6556de35b9025b8ef
yiuyu <217013277@stu.vtc.edu.hk>
2022-06-07 17:16:00 +0800

    successfully to open google fragment

--- app/src/main/java/com/example/mobileappproject/DetailPlaceActivity.kt
+++ app/src/main/java/com/example/mobileappproject/DetailPlaceActivity.kt
@@ -9,6 +9,7 @@ import android.widget.ImageView
 import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
 import com.example.mobileappproject.extensions.goToMainActivity
 import com.example.mobileappproject.lists.PlaceStatics
 import com.example.mobileappproject.lists.Task
@@ -44,6 +45,11 @@ class DetailPlaceActivity : AppCompatActivity() {
                     detailName.text = it.child("placeName").value.toString()
                     detailDesc.text = it.child("placeDesc").value.toString()
 //                    detailAddress.text = it.child("placeAddress").value.toString()
+                    detailWeather.text = it.child("placeWeather").value.toString()
+                    detailLat = it.child("placeLatitude").value.toString()
+                    detailLat = it.child("placeLongitude").value.toString()
+                    imageUrl = it.child("imageUrl").value.toString()
+                    //decor text with underline
                     val address = it.child("placeAddress").value.toString()
                     // Creating a Spannable String
                     // from the above string
@@ -53,10 +59,17 @@ class DetailPlaceActivity : AppCompatActivity() {
                     // the spannable string
                     addressUnderline.setSpan(UnderlineSpan(), 0, addressUnderline.length, 0)
                     detailAddress.text = addressUnderline
-                    detailWeather.text = it.child("placeWeather").value.toString()
-                    detailLat = it.child("placeLatitude").value.toString()
-                    detailLat = it.child("placeLongitude").value.toString()
-                    imageUrl = it.child("imageUrl").value.toString()
+                    detailAddress.setOnClickListener {
+                        val mapsFragment = MapsFragment()
+                        val fragment: Fragment? =
+                        supportFragmentManager.findFragmentByTag(MapsFragment::class.java.simpleName)
+                        if (fragment !is MapsFragment) {
+                            supportFragmentManager.beginTransaction()
+                                .add(R.id.fragmentContainer, mapsFragment, MapsFragment::class.java.simpleName)
+                                .commit()
+                        }
+                    }
+
                     Picasso.get().load(imageUrl).into(detailMainImage)
 
                     Log.i("firebase", "Got value ${it.value}")
