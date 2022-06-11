a4cf4ef1e5ffef36d1ff375819707f17ad5c3bca
hongyiuyu <217013277@stu.vtc.edu.hk>
2022-06-11 15:34:58 +0800

    Remove biometric in login page

--- app/src/main/java/com/example/mobileappproject/LoginActivity.kt
+++ app/src/main/java/com/example/mobileappproject/LoginActivity.kt
@@ -21,14 +21,18 @@ import com.google.firebase.auth.ktx.auth
 import com.google.firebase.ktx.Firebase
 
 class LoginActivity : AppCompatActivity() {
+    private lateinit var editTextEmailAddress: EditText
+    private lateinit var editTextPassword: EditText
     private lateinit var mGoogleSignInClient: GoogleSignInClient
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
 
+        editTextEmailAddress = findViewById(R.id.editTextEmailAddress)
+        editTextPassword = findViewById(R.id.editTextPassword)
+
         val loginBtn = findViewById<Button>(R.id.loginBtn)
-        val biometricLoginButton = findViewById<ImageButton>(R.id.biometricBtn)
         val googleSignIn = findViewById<SignInButton>(R.id.googleSignInBtn)
 
         loginBtn.setOnClickListener{
@@ -48,17 +52,13 @@ class LoginActivity : AppCompatActivity() {
             signInGoogle()
         }
 
-        //Biometric
-        biometricLoginButton.setOnClickListener { openBiometricAuth() }
     }
 
     // Email and password login
     private fun emailSignIn() {
-        val editTextEmailAddress = findViewById<EditText>(R.id.editTextEmailAddress)
-        val editTextPassword = findViewById<EditText>(R.id.editTextPassword)
         val email = editTextEmailAddress.text.toString()
         val password = editTextPassword.text.toString()
-        var (validEmail, validPassword) = checkValidForm(
+        val (validEmail, validPassword) = checkValidForm(
             email,
             editTextEmailAddress,
             password,
@@ -138,13 +138,6 @@ class LoginActivity : AppCompatActivity() {
         }
     }
 
-    private fun openBiometricAuth() {
-        val onSucceeded = Runnable {
-            ActivityChanger().goToMainActivity(this)
-        }
-        BiometricAuth().biometricAuth(this, this, onSucceeded)
-    }
-
     private fun updateUI(account: GoogleSignInAccount){
         val credential = GoogleAuthProvider.getCredential(account.idToken,null)
         Firebase.auth.signInWithCredential(credential).addOnCompleteListener {task->
@@ -157,7 +150,6 @@ class LoginActivity : AppCompatActivity() {
 
     override fun onStart() {
         super.onStart()
-        val biometricBtn = findViewById<ImageButton>(R.id.biometricBtn)
         val tvLoggedInText = findViewById<TextView>(R.id.tvLoggedInText)
         val tvLoggedInEmail = findViewById<TextView>(R.id.tvLoggedInEmail)
         val tvAskIfAccount = findViewById<TextView>(R.id.tvAskIfAccountExisted)
@@ -165,22 +157,28 @@ class LoginActivity : AppCompatActivity() {
 
         val user = Firebase.auth.currentUser
 
-        goToRegisterBtn.setOnClickListener{
-            ActivityChanger().goToRegisterActivity(this)
-            finish()
-            Toast.makeText(this,"go to Login Page",Toast.LENGTH_SHORT).show()
-        }
-
         if (user == null) {
-            biometricBtn.visibility = View.GONE
             tvLoggedInText.visibility = View.GONE
             tvLoggedInEmail.visibility = View.GONE
             tvAskIfAccount.text = getString(R.string.do_not_have_an_account)
 
+            goToRegisterBtn.setOnClickListener{
+                ActivityChanger().goToRegisterActivity(this)
+                finish()
+                Toast.makeText(this,"go to Register Page",Toast.LENGTH_SHORT).show()
+            }
         } else {
+            editTextEmailAddress.setText(user.email)
             tvLoggedInEmail.text = user.email
             tvAskIfAccount.text = getString(R.string.want_to_sign_in_with_another_account)
             goToRegisterBtn.text = getString(R.string.logout)
+
+            goToRegisterBtn.setOnClickListener{
+                Firebase.auth.signOut()
+                ActivityChanger().goToRegisterActivity(this)
+                finish()
+                Toast.makeText(this,"Logout and go to Register Page",Toast.LENGTH_SHORT).show()
+            }
         }
     }
 
