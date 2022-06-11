a0459196719e2f102960c1e55588719684a0baae
hongyiuyu <217013277@stu.vtc.edu.hk>
2022-06-11 15:38:08 +0800

    trying to implement test for ActivityChangerTest.kt

--- app/src/test/java/com/example/mobileappproject/extensions/ActivityChangerTest.kt
+++ app/src/test/java/com/example/mobileappproject/extensions/ActivityChangerTest.kt
@@ -1,13 +1,25 @@
 package com.example.mobileappproject.extensions
 
+import android.app.Activity
+import android.content.Context
+import android.content.Intent
 import org.junit.Test
 
 import org.junit.Assert.*
+import org.mockito.Mockito
+import org.mockito.Mockito.`when`
+import org.mockito.MockitoAnnotations
 
 internal class ActivityChangerTest {
 
     @Test
     fun goToAddPlaceActivity() {
+//        val context = Mockito.mock(Context::class.java)
+//        val intent = Mockito.mock(Intent::class.java)
+//        val activity = Mockito.mock(Activity::class.java)
+//
+//
+//        `when`(context.startActivity(intent)).thenReturn()
     }
 
     @Test
